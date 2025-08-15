31b58d233a0f1f3a9c3c648272b693d3b48ab236
Marv41 <127249962+Marv41@users.noreply.github.com>
2025-08-15 11:13:35 -0600

    Add files via upload

---
+++ easy_bid_game/main.py
@@ -0,0 +1,21 @@
+import art
+import os
+
+print(art.logo)
+bids = {}
+
+more_bidders = True
+while more_bidders:
+    name = input("What is your name? ")
+    bid = input("What is your bit? $")
+    bids[name] = bid
+    yes_or_no = input("Are there any other bidders? Type 'yes' or 'no'.")
+
+
+    if yes_or_no == "no":
+        more_bidders = False
+        winner = max(bids, key = bids.get)
+        print(f"The Winner is {winner} with a bid of ${bids[winner]} ")
+    else:
+        #os.system('cls' if os.name == 'nt' else 'clear')
+        print("\n" * 20)
