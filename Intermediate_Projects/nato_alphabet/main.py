6e1f45944b02a75b5351599ac45191db75f27ffd
Valenzuela <Mario.Valenzuela@dish.com>
2025-08-25 11:17:00 -0600

    added json directory and updated nato alphabet

--- Intermediate_Projects/nato_alphabet/main.py
+++ Intermediate_Projects/nato_alphabet/main.py
@@ -1,14 +1,17 @@
+import pandas
+
+#Example
 student_dict = {
-    "student": ["Angela", "James", "Lily"], 
+    "student": ["Angela", "James", "Lily"],
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
+#How to Loop through dictionaries:
 for (key, value) in student_dict.items():
     #Access key and value
     pass
 
-import pandas
+#Create Data Frame from dictionary
 student_data_frame = pandas.DataFrame(student_dict)
 
 #Loop through rows of a data frame
@@ -18,14 +21,20 @@ for (index, row) in student_data_frame.iterrows():
     pass
 
 # Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+# {new_key : new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+
 nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
-nato_dict = {row.letter:row.code for (index,row) in nato_alphabet.iterrows()}
+nato_dict = {row.letter : row.code for (index,row) in nato_alphabet.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Please Type Any Word: ").upper()
 
-phonetic_list = [nato_dict[letter] for letter in user_input]
-print(phonetic_list)
+while True:
+    user_input = input("Please Type Any Word: ").upper()
+
+    try:
+        phonetic_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letter in the alphabet please")
+    else:
+        print(phonetic_list)
+        break
